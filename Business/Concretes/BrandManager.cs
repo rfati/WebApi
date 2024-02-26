c65c6ff9f8b9d5ac38aa8812ddaaed2ad30685c0
rıfat işler <rifat.isler@hotmail.com>
2024-02-27 00:04:12 +0300

    Commit 2

--- Business/Concretes/BrandManager.cs
+++ Business/Concretes/BrandManager.cs
@@ -36,7 +36,22 @@ namespace Business.Concretes
 
         public List<GetAllBrandResponse> GetAll()
         {
-            throw new NotImplementedException();
+            List<Brand> brands = _brandDal.GetAll();
+
+            List<GetAllBrandResponse> getAllBrandResponses = new List<GetAllBrandResponse>();
+
+            foreach (var brand in brands)
+            {
+                GetAllBrandResponse getAllBrandResponse = new GetAllBrandResponse();    
+                getAllBrandResponse.Name = brand.Name;  
+                getAllBrandResponse.Id = brand.Id;
+                getAllBrandResponse.CreatedDate = brand.CreatedDate;
+
+                getAllBrandResponses.Add(getAllBrandResponse);
+                
+            }
+
+            return getAllBrandResponses;
         }
     }
 }
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,9 @@
 
+using Business.Abstracts;
+using Business.Concretes;
+using DataAccess.Abstracts;
+using DataAccess.Concretes;
+
 namespace WebApi
 {
     public class Program
@@ -13,6 +18,8 @@ namespace WebApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddSingleton<IBrandService, BrandManager>();
+            builder.Services.AddSingleton<IBrandDal, BrandDal>();
 
             var app = builder.Build();
 
